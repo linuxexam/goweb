0ce8c08716130b51e0006edaa1bf1b31033eb3af
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2021-11-28 18:15:27 -0800

    init commit

---
+++ cert/cert.go
@@ -0,0 +1,58 @@
+package cert
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+)
+
+func CheckCert(url string) (string, error) {
+	if !strings.Contains(url, ":") {
+		url += ":443"
+	}
+	conn, err := tls.Dial("tcp", url, nil)
+	if err != nil {
+		return err.Error() + "\n", err
+	}
+	defer conn.Close()
+
+	cs := conn.ConnectionState()
+	peerCert := cs.PeerCertificates[0]
+	return sprintX509(peerCert), nil
+}
+
+func sprintX509(cert *x509.Certificate) string {
+	s := new(strings.Builder)
+	fmt.Fprintf(s, "subject:    %s\n", cert.Subject.CommonName)
+	fmt.Fprintf(s, "SAN:        %s\n", cert.DNSNames)
+	fmt.Fprintf(s, "Not before: %s\n", sprintTime(cert.NotBefore))
+	fmt.Fprintf(s, "Not after:  %s\n", sprintTime(cert.NotAfter))
+	return s.String()
+}
+
+func sprintTime(t time.Time) string {
+	return fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
+}
+
+func TestCheckCert(t *testing.T) {
+	chOut := make(chan string)
+	urls := []string{
+		"mail.google.com",
+		"www.bcit.ca",
+		"notexist.cm",
+	}
+	for _, url := range urls {
+		go func(url string) {
+			var r string
+			defer func() { chOut <- (url + ":\n" + r) }()
+			r, _ = CheckCert(url)
+		}(url)
+	}
+
+	for range urls {
+		fmt.Print(<-chOut)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/linuxexam/goweb
+
+go 1.17
